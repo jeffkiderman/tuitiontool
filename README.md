d243c8f544d0ba22b701dd40eae6acce72cce49d
Miki Friedmann <mjfriedmann@gmail.com>
2016-06-23 23:23:15 -0400

    My first Webserver using express

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # tuitiontool
-To run: `npm start`
+
+#### Run
+To run, execute `npm start`.
+This will start the webserver.  Then navigate your browser to `localhost:3000`.  This doesn't do anything interesting right now.  Essentially just a hello world.
+
+#### Typechecker
+Flow is a typechecker built on top of javascript. Include `// @flow` at the top of your js file to allow flow to analyze your file.
+To check for flow errors, run
+`npm run-script flow`
+
+#### File structure
+Now the main app runs out of the root index.js.  Not sure what the correct structure is, but I plan to read about that in the next few days and adapt.
--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-require('./app/index')
+// index.js
+// @flow
+require('./app/index');
+
+const path = require('path');
+// webserver
+const express = require('express');
+// i think this is the template engine (to render layouts)
+const exphbs = require('express-handlebars');
+// localhost:3000
+const port = 3000;
+
+app = express();
+// guessing this registers the template engine with the webserver
+// main has the entire http response
+app.engine('.hbs', exphbs({
+  defaultLayout: 'main',
+  extname: '.hbs',
+  layoutsDir: path.join(__dirname, 'views/layouts'),
+}));
+// dunno
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, 'views'));
+
+// webserver responds to root requests (localhost:3000)
+app.get('/', (request, response) => {
+  // home.hbs is a template with a name variable that needs assignment
+  response.render('home', {
+    name: 'John'
+  })
+});
+
+app.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+  console.log(`server is listening on ${port}`)
+})
