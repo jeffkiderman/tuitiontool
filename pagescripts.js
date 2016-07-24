a2b6ad9be7802f304ce07bf79c9ea5221ab0d902
gkiderman <jeff@ajeproject.org>
2016-07-23 23:18:36 -0400

    Made pageload and dataload happen in the same step

--- pagescripts.js
+++ pagescripts.js
@@ -5,7 +5,8 @@ function loadData(){
     var req = new XMLHttpRequest();
     req.open("GET", "http://localhost:3000/sourcedata", true);
     req.addEventListener("load",function(){
-                         data = JSON.parse(req.responseText);
+                            data = JSON.parse(req.responseText);
+                            createMenu();
                          });
     req.send(null);
 }
