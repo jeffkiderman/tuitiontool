12fb6857b236c9d2f3c23f1952b84eaa0d250d10
gkiderman <jeff@ajeproject.org>
2016-08-11 10:45:04 -0400

    worked out the rest of the bugs, I think

--- app/public/js/SchoolScripts.js
+++ app/public/js/SchoolScripts.js
@@ -55,59 +55,38 @@ const SchoolScripts = {
         security: getSecurityProps(d),
         building: getBuildingProps(d),
         discount: getDiscountProps(d),
-        optionalLunch: d[Cols.optionalLunch]
+        optionalLunch: ~~d[Cols.optionalLunch]
       };
       // since totalTuition uses lots of funcions defined in school,
       // add it to school after the rest of the school definition is created
       school.totalTuition = function(kids, yearsInSchool, dateRegistered) {
-        if(school.basicInfo.isTuitOnline() == undefined) {
+          if(school.basicInfo.isTuitOnline() == false) {
           return "Tuition information not available for this school";
         }
         var total = 0;
+        //console.log(total + " line 67");
         total += school.baseTuition.baseSubtotal(kids);
+        // console.log(total + " line 69");
         total += school.activities.activitiesSubtotal(kids);
+        // console.log(total + " line 71");
         total += school.registration.registrationSubtotal(kids,yearsInSchool,dateRegistered);
+        //  console.log(total + " line 73");
         total += school.gradFee.gradFeeSubtotal(kids);
+        //  console.log(total + " line 75");
         total += school.ptaFees.ptaFeeSubtotal(kids);
+        //  console.log(total + " line 77");
         total += school.familyCommitments.familyCommitmentsSubtotal();
+        //  console.log(total + " line 79");
         total += school.security.securityFeeSubtotal(kids);
+        //  console.log(total + " line 81");
         total += school.building.buildingSubtotal(kids,yearsInSchool);
+        //  console.log(total + " line 83");
         total += school.discount.discountSubtotal(kids);
+        //  console.log(total + " line 85");
         return "$"+total.toLocaleString();
       };
     return school;
   },
-    /*
-    //Calculates total per-family costs for the school
-    school.perFamily = (~~d[88])+(~~d[91])+(~~d[92])+(~~d[93])+(~~d[94]);
-    //Calculates total per-child costs based on array of children passed in as parameter
-    school.perChild = function(children){
-        var perStudent = [];
-        for(var i = 0; i < children.length; i++){
-            var t = 0;
-            var gr = children[i].grade;
-            t = (~~d[(37+gr)])+(~~d[(63+gr)]);
-            t += (~~d[83])+(~~d[84])+(~~d[85])+(~~d[86])+(~~d[87]);
-            if(gr === 8){t += (~~d[81]);}
-            else if(gr === 0){t += (~~d[80]);}
-            else if(gr === 12){t += (~~d[82]);}
-            perStudent.push(t);
-        }
-        return perStudent;
-    };
-
-    school.registration = function(returning,children){
-        if(returning){
-            return (~~d[77])*children.length + (~~d[90]);
-        }
-        else{
-            return (~~d[76])*children.length + (~~d[89]);
-        }
-    };
-    document.getElementById("print-tuition").innerHTML="<h2>"+SchoolScripts.calculateTuition(school, SchoolScripts.createFamilyObj())+"</h2>";
-    console.log(school.baseTuition.getBase(3));
-    //console.log(calculateTuition(school,createFamilyObj()));
-  },*/
 
 //Creates object representing a family, with array of children
   createFamilyObj: function() {
@@ -131,16 +110,6 @@ const SchoolScripts = {
       ];
       return family;
   },
-
-  //Calculates total tuition bill for a given family and school
-  /*calculateTuition: function(sch, fam) {
-      /*if(sch.tuitionSaved == undefined){
-          return "Tuition information not available for this school";
-      }
-      return "$"+(sch.perChild(fam.kids).reduce(function(a,b){return a+b;},0) + sch.perFamily + sch.registration(fam.returning,fam.kids)).toLocaleString();*/
-    //if(sch.basicInfo.isTuitOnline() == undefined) {return "Tuition information not available for this school";}
-    //return "$"+(sch.totalTuition(fam.kids,0,20160422)).toLocaleString();
- // }
 };
 
 function getBasicInfoProps(d){
@@ -152,7 +121,10 @@ function getBasicInfoProps(d){
     const schoolNotes = d[Cols.schoolNotes];
     const tuitionNotes = d[Cols.tuitionNotes];
     return {
-      isTuitOnline: function(){return tuitOnline}
+      isTuitOnline: function(){
+          if(tuitOnline == "Yes"){return true;}
+          return false;
+      }
     };
 }
 function getContactProps(d){
@@ -175,38 +147,38 @@ function getInSessionProps(d){
 function getStudentsProps(d){
     const grades = d[Cols.grades];
     const fte = d[Cols.fte];
-    const enrollByGrade = [d[Cols.enrollpreK],
-                   d[Cols.enrollK],
-                   d[Cols.enroll1st],
-                   d[Cols.enroll2nd],
-                   d[Cols.enroll3rd],
-                   d[Cols.enroll4th],
-                   d[Cols.enroll5th],
-                   d[Cols.enroll6th],
-                   d[Cols.enroll7th],
-                   d[Cols.enroll8th],
-                   d[Cols.enroll9th],
-                   d[Cols.enroll10th],
-                   d[Cols.enroll11th],
-                   d[Cols.enroll12th]];
+    const enrollByGrade = [~~d[Cols.enrollpreK],
+                   ~~d[Cols.enrollK],
+                   ~~d[Cols.enroll1st],
+                   ~~d[Cols.enroll2nd],
+                   ~~d[Cols.enroll3rd],
+                   ~~d[Cols.enroll4th],
+                   ~~d[Cols.enroll5th],
+                   ~~d[Cols.enroll6th],
+                   ~~d[Cols.enroll7th],
+                   ~~[Cols.enroll8th],
+                   ~~d[Cols.enroll9th],
+                   ~~d[Cols.enroll10th],
+                   ~~d[Cols.enroll11th],
+                   ~~d[Cols.enroll12th]];
     return {
       getEnrollment: function(grade){return enrollbyGrade[grade];}
     };
 }
 function getBaseTuitionProps(d){
-    const baseByGrade = [d[Cols.baseK],
-                  d[Cols.base1st],
-                  d[Cols.base2nd],
-                  d[Cols.base3rd],
-                  d[Cols.base4th],
-                  d[Cols.base5th],
-                  d[Cols.base6th],
-                  d[Cols.base7th],
-                  d[Cols.base8th],
-                  d[Cols.base9th],
-                  d[Cols.base10th],
-                  d[Cols.base11th],
-                  d[Cols.base12th]];
+    const baseByGrade = [~~d[Cols.baseK],
+                  ~~d[Cols.base1st],
+                  ~~d[Cols.base2nd],
+                  ~~d[Cols.base3rd],
+                  ~~d[Cols.base4th],
+                  ~~d[Cols.base5th],
+                  ~~d[Cols.base6th],
+                  ~~d[Cols.base7th],
+                  ~~d[Cols.base8th],
+                  ~~d[Cols.base9th],
+                  ~~d[Cols.base10th],
+                  ~~d[Cols.base11th],
+                  ~~d[Cols.base12th]];
     const getBase = function(grade){return baseByGrade[grade];};
     return {
       getBase: getBase,
@@ -221,19 +193,19 @@ function getBaseTuitionProps(d){
     };
 }
 function getInclusionTuitionProps(d){
-    const inclusionByGrade = [d[Cols.inclusionK],
-                      d[Cols.inclusion1st],
-                      d[Cols.inclusion2nd],
-                      d[Cols.inclusion3rd],
-                      d[Cols.inclusion4th],
-                      d[Cols.inclusion5th],
-                      d[Cols.inclusion6th],
-                      d[Cols.inclusion7th],
-                      d[Cols.inclusion8th],
-                      d[Cols.inclusion9th],
-                      d[Cols.inclusion10th],
-                      d[Cols.inclusion11th],
-                      d[Cols.inclusion12th]];
+    const inclusionByGrade = [~~d[Cols.inclusionK],
+                      ~~d[Cols.inclusion1st],
+                      ~~d[Cols.inclusion2nd],
+                      ~~d[Cols.inclusion3rd],
+                      ~~d[Cols.inclusion4th],
+                      ~~d[Cols.inclusion5th],
+                      ~~d[Cols.inclusion6th],
+                      ~~d[Cols.inclusion7th],
+                      ~~d[Cols.inclusion8th],
+                      ~~d[Cols.inclusion9th],
+                      ~~d[Cols.inclusion10th],
+                      ~~d[Cols.inclusion11th],
+                      ~~d[Cols.inclusion12th]];
     return {
       getInclusion: function(grade){return getInclusion[grade];},
       inclusionSubtotal: function(kids){
@@ -246,19 +218,19 @@ function getInclusionTuitionProps(d){
   };
 }
 function getActivitiesProps(d){
-    const activitiesByGrade = [d[Cols.activitiesK],
-                       d[Cols.activities1st],
-                       d[Cols.activities2nd],
-                       d[Cols.activities3rd],
-                       d[Cols.activities4th],
-                       d[Cols.activities5th],
-                       d[Cols.activities6th],
-                       d[Cols.activities7th],
-                       d[Cols.activities8th],
-                       d[Cols.activities9th],
-                       d[Cols.activities10th],
-                       d[Cols.activities11th],
-                       d[Cols.activities12th]];
+    const activitiesByGrade = [~~d[Cols.activitiesK],
+                       ~~d[Cols.activities1st],
+                       ~~d[Cols.activities2nd],
+                       ~~d[Cols.activities3rd],
+                       ~~d[Cols.activities4th],
+                       ~~d[Cols.activities5th],
+                       ~~d[Cols.activities6th],
+                       ~~d[Cols.activities7th],
+                       ~~d[Cols.activities8th],
+                       ~~d[Cols.activities9th],
+                       ~~d[Cols.activities10th],
+                       ~~d[Cols.activities11th],
+                       ~~d[Cols.activities12th]];
     const getActivities = function(grade){return activitiesByGrade[grade];};
     return {
       getActivities: getActivities,
@@ -272,13 +244,13 @@ function getActivitiesProps(d){
   };
 }
 function getRegistrationProps(d) {
-  const regPerKidNew = d[Cols.regPerKidNew];
-  const regPerKidReturn = d[Cols.regPerKidReturn];
-  const regDate = d[Cols.regDate];
-  const regLateFee = d[Cols.regLateFee];
-  const regPerFamNew = d[Cols.regPerFamNew];
-  const regPerFamReturn = d[Cols.regPerFamReturn];
-  const regKDiscount = d[Cols.regKDiscount];
+  const regPerKidNew = ~~d[Cols.regPerKidNew];
+  const regPerKidReturn = ~~d[Cols.regPerKidReturn];
+  const regDate = ~~d[Cols.regDate];
+  const regLateFee = ~~d[Cols.regLateFee];
+  const regPerFamNew = ~~d[Cols.regPerFamNew];
+  const regPerFamReturn = ~~d[Cols.regPerFamReturn];
+  const regKDiscount = ~~d[Cols.regKDiscount];
 
   var regIsLate = function(dateRegistered){
       if(regDate == undefined) return false;
@@ -315,8 +287,8 @@ function getRegistrationProps(d) {
     };
   }
 function getGradFeeProps(d){
-    const gradFee8th = d[Cols.gradFee8th];
-    const gradFee12th = d[Cols.gradFee12th];
+    const gradFee8th = ~~d[Cols.gradFee8th];
+    const gradFee12th = ~~d[Cols.gradFee12th];
     const getGradFee = function(grade){
         if(grade == 8){ return gradFee8th; }
         if(grade == 12){ return gradFee12th; }
@@ -334,8 +306,8 @@ function getGradFeeProps(d){
     };
 }
 function getPtaFeesProps(d){
-    const ptaPerKid = d[Cols.ptaPerKid];
-    const ptaPerFam = d[Cols.ptaPerFam];
+    const ptaPerKid = ~~d[Cols.ptaPerKid];
+    const ptaPerFam = ~~d[Cols.ptaPerFam];
     return {
       ptaFeeSubtotal: function(kids){
         var sub = ptaPerFam;
@@ -345,16 +317,16 @@ function getPtaFeesProps(d){
   };
 }
 function getFamilyCommitmentsProps(d){
-    const scholarship = d[Cols.scholarshipPerKid];
-    const familyObligation = d[Cols.familyObligation];
-    const scripPerFam = d[Cols.scripPerFam];
+    const scholarship = ~~d[Cols.scholarshipPerKid];
+    const familyObligation = ~~d[Cols.familyObligation];
+    const scripPerFam = ~~d[Cols.scripPerFam];
     return {
       familyCommitmentsSubtotal : function(){return scholarship + familyObligation + scripPerFam;}
   };
 }
 function getSecurityProps(d){
-    const securityPerKid = d[Cols.securityPerKid];
-    const securityPerFam = d[Cols.securityPerFam];
+    const securityPerKid = ~~d[Cols.securityPerKid];
+    const securityPerFam = ~~d[Cols.securityPerFam];
     return {
       securityFeeSubtotal : function(kids){
         var sub = securityPerFam;
@@ -364,23 +336,23 @@ function getSecurityProps(d){
     };
 }
 function getBuildingProps(d){
-    const buildingPerKid = d[Cols.buildingPerKid];
-    const buildingPerFamAnnual = d[Cols.buildingPerFamAnnual];
-    const buildingPerFamByYr = [d[Cols.buildingPerFamYr1],
-                        d[Cols.buildingPerFamYr2],
-                        d[Cols.buildingPerFamYr3],
-                        d[Cols.buildingPerFamYr4],
-                        d[Cols.buildingPerFamYr5],
-                        d[Cols.buildingPerFamYr6],
-                        d[Cols.buildingPerFamYr7],
-                        d[Cols.buildingPerFamYr8]];
+    const buildingPerKid = ~~d[Cols.buildingPerKid];
+    const buildingPerFamAnnual = ~~d[Cols.buildingPerFamAnnual];
+    const buildingPerFamByYr = [~~d[Cols.buildingPerFamYr1],
+                        ~~d[Cols.buildingPerFamYr2],
+                        ~~d[Cols.buildingPerFamYr3],
+                        ~~d[Cols.buildingPerFamYr4],
+                        ~~d[Cols.buildingPerFamYr5],
+                        ~~d[Cols.buildingPerFamYr6],
+                        ~~d[Cols.buildingPerFamYr7],
+                        ~~d[Cols.buildingPerFamYr8]];
     return {
       buildingSubtotal: function(kids,yearsInSchool){
         var sub = buildingPerFamAnnual;
         sub += (buildingPerKid * kids.length);
         // FIXME:  Jeff, I edited this line from year - 1 to yearsInSchool - 1
         // either fix or remove this comment :)
-        if(yearsInSchool <= buildingPerFamByYr.length){sub += buildingPerFamByYr[yearsInSchool-1];}
+        if(yearsInSchool <= buildingPerFamByYr.length){sub += buildingPerFamByYr[yearsInSchool];}
         return sub;
     }
   };
@@ -388,10 +360,10 @@ function getBuildingProps(d){
 function getDiscountProps(d){
     const discountMultiKid = [0,
                       0,
-                      d[Cols.discount2Kids],
-                      d[Cols.discount3Kids],
-                      d[Cols.discount4Kids],
-                      d[Cols.discount5Kids]];
+                      ~~d[Cols.discount2Kids],
+                      ~~d[Cols.discount3Kids],
+                      ~~d[Cols.discount4Kids],
+                      ~~d[Cols.discount5Kids]];
     return {
       discountSubtotal : function(kids){return (-1)*(discountMultiKid[Math.max(kids.length,5)])}
     };
